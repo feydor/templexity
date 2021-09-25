e2ac3d44e56f212ba76e8ba539595b99132938c7
Victor Reyes <vreyes85739@outlook.com>
2021-09-24 19:05:17 -0700

    Add threejs and animate a cube in a react component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import * as THREE from "three";
+
+// three.js globals
+let camera, scene, renderer;
+let geometry, material, mesh;
 
 function App() {
+  const mountRef = useRef(null);
+
+  // setup three.js
+  useEffect(() => {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+	  camera.position.z = 1;
+
+  	scene = new THREE.Scene();
+
+  	geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  	material = new THREE.MeshNormalMaterial();
+
+  	mesh = new THREE.Mesh(geometry, material);
+  	scene.add(mesh);
+
+  	renderer = new THREE.WebGLRenderer({ antialias: true });
+  	renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(renderer.domElement);
+    window.addEventListener("resize", onWindowResize, false);
+    animate();
+
+    return () => mountRef.current.removeChild(renderer.domElement);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Three" ref={mountRef}></div>
   );
+}
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  mesh.rotation.x += 0.02;
+  mesh.rotation.y += 0.02;
+  renderer.render(scene, camera);
+}
+
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 export default App;
