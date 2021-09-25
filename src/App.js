eb3b122ce6a15a7217b4b12b310c46fd6ba375c0
Victor Reyes <vreyes85739@outlook.com>
2021-09-24 22:13:20 -0700

    Change render to scrollable graph.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useRef } from 'react';
-import * as THREE from "three";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // three.js globals
 let camera, scene, renderer;
-let geometry, material, mesh;
+let geometry, material, mesh, grid;
 
 function App() {
   const mountRef = useRef(null);
 
-  // setup three.js
+  // setup three.js, mount renderer, animate, demount
   useEffect(() => {
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-	  camera.position.z = 1;
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	  camera.position.z = 500;
 
   	scene = new THREE.Scene();
 
@@ -23,10 +24,22 @@ function App() {
   	mesh = new THREE.Mesh(geometry, material);
   	scene.add(mesh);
 
+    const axesHelper = new THREE.AxesHelper(100);
+    scene.add(axesHelper);
+
+    const size = 400;
+    const divisions = 40;
+    grid = new THREE.GridHelper(size, divisions, 0x0000ff, 0x808080);
+    grid.position.y = - 50;
+    grid.rotation.x = 90 * Math.PI / 180;
+    scene.add(grid);
+
   	renderer = new THREE.WebGLRenderer({ antialias: true });
   	renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
     window.addEventListener("resize", onWindowResize, false);
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
     animate();
 
     return () => mountRef.current.removeChild(renderer.domElement);
