1363943b65ce46b13b234e011972d72eb4ab8638
feydor <vreyes85739@outlook.com>
2021-10-06 11:40:54 -0700

    Refactor init page functions into methods of a prototype Page.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,120 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import Solver from './math.js';
 
 // three.js globals
-let renderer, currScene = 0;
-const NUM_SCENES = 3;
-let scenes = []; // contains an object with a scene and a camera
+let renderer, currPage = 0;
+const NUM_PAGES = 3;
+let pages = []; // contains an object with a scene and a camera
+
+class Page {
+  constructor(scene, camera, npage) {
+    this.scene = scene;
+    this.camera = camera;
+    this.npage = npage;
+  }
+
+  render() {
+    renderer.render(this.scene, this.camera);
+  }
+
+  // currPageIndex is 0-indexed
+  isCurrent(currPageIndex) {
+    return (currPageIndex + 1) === this.npage;
+  }
+}
+
+class Page1 extends Page {
+  constructor() {
+    const {scene, camera} = makeScene();
+
+    const points = [];
+    {
+      const ticks = 999;
+      let x = 0;
+      let y = 0;
+      let r = 0.1;
+      const a = 1.00198;
+      for (let t = 0; t < ticks; t += 0.1) {
+        r *= a;
+        x = r * Math.cos(t);
+        y = r * Math.sin(t);
+        points.push(new THREE.Vector3(x, y, 0));
+      }
+    }
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const material = new THREE.LineBasicMaterial({ color: 0x00d333, linewidth: 20 });
+    const mesh = new THREE.Line(geometry, material);
+    scene.add(mesh);
+    scene.mesh = mesh;
+    camera.position.set(0, 0, 2);
+    camera.lookAt(0, 0, 0);
+
+    super(scene, camera, 1);
+  }
+
+  animate(time) {
+    this.scene.mesh.rotation.z = time * 1;
+  }
+}
+
+class Page2 extends Page {
+  constructor() {
+    const {scene, camera} = makeScene();
+    camera.position.z = 3333;
+    camera.lookAt(0, 0, 0);
+
+    const geometry = new THREE.IcosahedronGeometry(666);
+    const material = new THREE.MeshBasicMaterial({ color:0x00d333, wireframe: true });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.flatShading = true;
+    scene.add(mesh);
+    const light = new THREE.AmbientLight( { color: 0x404040, intensity: 1.2 } );
+    scene.add(light);
+    scene.mesh = mesh;
+
+    super(scene, camera, 2);
+  }
+
+  animate(time) {
+    this.scene.mesh.rotation.x = time * -0.1;
+    this.scene.mesh.rotation.y = time * 0.1;
+  }
+}
+
+class Page3 extends Page {
+  constructor() {
+    const {scene, camera} = makeScene();
+
+    const points = [];
+    {
+      const ticks = 999;
+      let x = 0;
+      let y = 0;
+      let r = 0.1;
+      const a = 1.00198;
+      for (let t = 0; t < ticks; t += 0.1) {
+        r *= a;
+        x = r * Math.cos(t);
+        y = r * Math.sin(t);
+        points.push(new THREE.Vector3(x, y, t));
+      }
+    }
+
+    const material = new THREE.LineBasicMaterial({ color: 0x00d333, linewidth: 20 });
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const mesh = new THREE.Line(geometry, material);
+    scene.add(mesh);
+    scene.mesh = mesh;
+    camera.position.set( 500, 1000, 200 );
+    camera.lookAt( 0, 0, 0 );
+
+    super(scene, camera, 3);
+  }
+
+  animate(time) {
+    this.scene.mesh.rotation.z = time * 1;
+  }
+}
 
 function App() {
   const mountRef = useRef(null);
@@ -18,27 +129,27 @@ function App() {
   // setup three.js, mount renderer, animate, demount
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  scenes[0] = init_scene1();
-  scenes[1] = init_scene2();
-  scenes[2] = init_scene3();
+  pages[0] = new Page1();
+  pages[1] = new Page2();
+  pages[2] = new Page3();
 
   useEffect(() => {
     mountRef.current.appendChild(renderer.domElement);
-    window.addEventListener("resize", onWindowResize(scenes[currScene]), false);
+    window.addEventListener("resize", onWindowResize(pages[currPage].camera), false);
 
-    const changeScene = () => {
-      if (++currScene >= NUM_SCENES) {
-        currScene = 0;
+    const changePage = () => {
+      if (++currPage >= NUM_PAGES) {
+        currPage = 0;
       }
     };
 
     render();
 
     const currentRef = mountRef.current;
-    document.getElementById('button').addEventListener('click', changeScene);
+    document.getElementById('button').addEventListener('click', changePage);
 
     return () => {
-      document.getElementById('button').removeEventListener('click', changeScene);
+      document.getElementById('button').removeEventListener('click', changePage);
       currentRef.removeChild(renderer.domElement);
     }
   });
@@ -52,83 +163,11 @@ function App() {
 }
 
 // P1: arithmetic spirals infinitely inwards towards Zero
-const onWindowResize = ({camera}) => {
+const onWindowResize = (camera) => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
-
-function init_scene1() {
-  const {scene, camera} = makeScene();
-
-  const points = [];
-  {
-    const ticks = 999;
-    let x = 0;
-    let y = 0;
-    let r = 0.1;
-    const a = 1.00198;
-    for (let t = 0; t < ticks; t += 0.1) {
-      r *= a;
-      x = r * Math.cos(t);
-      y = r * Math.sin(t);
-      points.push(new THREE.Vector3(x, y, 0));
-    }
-  }
-
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  const material = new THREE.LineBasicMaterial({ color: 0x00d333, linewidth: 20 });
-  const mesh = new THREE.Line(geometry, material);
-  scene.add(mesh);
-  scene.mesh = mesh;
-  camera.position.set(0, 0, 2);
-  camera.lookAt(0, 0, 0);
-  return {scene, camera};
-};
-
-function init_scene2() {
-  const {scene, camera} = makeScene();
-  camera.position.z = 3333;
-  camera.lookAt(0, 0, 0);
-
-  const geometry = new THREE.IcosahedronGeometry(666);
-  const material = new THREE.MeshBasicMaterial({ color:0x00d333, wireframe: true });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.flatShading = true;
-  scene.add(mesh);
-  const light = new THREE.AmbientLight( { color: 0x404040, intensity: 1.2 } );
-  scene.add(light);
-  scene.mesh = mesh;
-  return {scene, camera};
-};
-
-function init_scene3() {
-  const {scene, camera} = makeScene();
-
-  const points = [];
-  {
-    const ticks = 999;
-    let x = 0;
-    let y = 0;
-    let r = 0.1;
-    const a = 1.00198;
-    for (let t = 0; t < ticks; t += 0.1) {
-      r *= a;
-      x = r * Math.cos(t);
-      y = r * Math.sin(t);
-      points.push(new THREE.Vector3(x, y, t));
-    }
-  }
-
-  const material = new THREE.LineBasicMaterial({ color: 0x00d333, linewidth: 20 });
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  const mesh = new THREE.Line(geometry, material);
-  scene.add(mesh);
-  scene.mesh = mesh;
-  camera.position.set( 500, 1000, 200 );
-  camera.lookAt( 0, 0, 0 );
-  return {scene, camera};
-};
 
 function makeScene() {
   const scene = new THREE.Scene();
@@ -155,11 +194,6 @@ function makeScene() {
   return {scene, camera};
 };
 
-const renderScene = (a_scene) => {
-  const {scene, camera} = a_scene;
-  renderer.render(scene, camera);
-};
-
 function resizeRendererToDisplaySize(renderer) {
   const canvas = renderer.domElement;
   const width = canvas.clientWidth;
@@ -171,24 +205,17 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
-// TODO: Make a Scene class with:
-// - abstract animate method
-// - abstract init or constructor
-// - default render method
 const render = (time) => {
   time *= 0.001;
   resizeRendererToDisplaySize(renderer);
 
-  // animation updates here
-  scenes[0].scene.mesh.rotation.z = time * 1;
+  for (const page of pages) {
+    page.animate(time);
+    if (page.isCurrent(currPage)) {
+      page.render();
+    }
+  }
 
-  scenes[1].scene.mesh.rotation.x = time * -0.1;
-  scenes[1].scene.mesh.rotation.y = time * 0.1;
-  // camera.position.z = 3333;
-
-  scenes[2].scene.mesh.rotation.z = time * 1;
-
-  renderScene(scenes[currScene]);
   requestAnimationFrame(render);
 };
 
