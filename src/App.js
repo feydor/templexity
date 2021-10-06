d03475d74e6a6a04168e054fdedb8cb6d780c3f9
feydor <vreyes85739@outlook.com>
2021-10-05 21:49:25 -0700

    Fix cameras for each scene; fix build errors.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
@@ -7,9 +7,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import Solver from './math.js';
 
 // three.js globals
-let camera, renderer, currScene = 0;
+let renderer, currScene = 0;
 const NUM_SCENES = 3;
-let scenes = [];
+let scenes = []; // contains an object with a scene and a camera
 
 function App() {
   const mountRef = useRef(null);
@@ -19,7 +19,6 @@ function App() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   scenes[0] = init_scene1();
-  console.log(scenes[0]);
   scenes[1] = init_scene2();
   scenes[2] = init_scene3();
 
@@ -82,12 +81,15 @@ function init_scene1() {
   const mesh = new THREE.Line(geometry, material);
   scene.add(mesh);
   scene.mesh = mesh;
-  camera.position.z = 1;
+  camera.position.set(0, 0, 2);
+  camera.lookAt(0, 0, 0);
   return {scene, camera};
 };
 
 function init_scene2() {
   const {scene, camera} = makeScene();
+  camera.position.z = 3333;
+  camera.lookAt(0, 0, 0);
 
   const geometry = new THREE.IcosahedronGeometry(666);
   const material = new THREE.MeshBasicMaterial({ color:0x00d333, wireframe: true });
@@ -123,7 +125,7 @@ function init_scene3() {
   const mesh = new THREE.Line(geometry, material);
   scene.add(mesh);
   scene.mesh = mesh;
-  camera.position.set( 100, 100, 100 );
+  camera.position.set( 500, 1000, 200 );
   camera.lookAt( 0, 0, 0 );
   return {scene, camera};
 };
@@ -132,9 +134,6 @@ function makeScene() {
   const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.z = 2;
-  camera.position.set(0, 1, 2);
-  camera.lookAt(0, 0, 0);
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enablePan = false;
   controls.enableZoom = false;
@@ -161,28 +160,33 @@ const renderScene = (a_scene) => {
   renderer.render(scene, camera);
 };
 
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
 // TODO: Make a Scene class with:
 // - abstract animate method
 // - abstract init or constructor
 // - default render method
 const render = (time) => {
   time *= 0.001;
-  // resizeRendererToDisplaySize(renderer);
+  resizeRendererToDisplaySize(renderer);
 
   // animation updates here
-  {
-    scenes[0].scene.mesh.rotation.z = time * 0.1;
-  }
+  scenes[0].scene.mesh.rotation.z = time * 1;
 
-  {
-    scenes[1].scene.mesh.rotation.x = time * -0.01;
-    scenes[1].scene.mesh.rotation.y = time * 0.01;
-    // camera.position.z = 3333;
-  }
+  scenes[1].scene.mesh.rotation.x = time * -0.1;
+  scenes[1].scene.mesh.rotation.y = time * 0.1;
+  // camera.position.z = 3333;
 
-  {
-    scenes[2].scene.mesh.rotation.z = time * 0.1;
-  }
+  scenes[2].scene.mesh.rotation.z = time * 1;
 
   renderScene(scenes[currScene]);
   requestAnimationFrame(render);
